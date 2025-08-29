a659f01391223789ed75633ef22639eb84a820b2
JonathanRL-dev <nathan020407@gmail.com>
2025-08-29 14:09:42 +0700

    PerkenalanDiri.cpp

--- PerkenalanDiri.cpp
+++ PerkenalanDiri.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hi, my name is Jonathan Raphael Liongtoro and my favourite programming language is c ");
+    printf("Hi, my name is Jonathan Raphael Liongtoro and my favourite programming language is C ");
 }
